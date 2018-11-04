d63d86fec841442e5c64293c769f9898a1471695
MichaelCastillo <michael.castillo.p96@gmail.com>
2018-11-04 19:38:08 -0300

    Se agrega el filtro pasabajos, se activa al calcular la transformada de fourier

--- src/FFT/FFT.py
+++ src/FFT/FFT.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 import scipy.io.wavfile as wavfile
 import scipy
+from scipy.signal import butter, lfilter, freqz
 import scipy.fftpack
 import numpy as np
 from matplotlib import pyplot as plt
@@ -23,6 +24,7 @@ def calculateFFT(fs_rate, signal, flag):
     print ("Timestep between samples Ts", Ts)
     t = scipy.arange(0, secs, Ts) # time vector as scipy arange field / numpy.ndarray
     FFT = abs(scipy.fft(signal))
+    filter(signal,3.667,30.0,7)
     freqs = scipy.fftpack.fftfreq(signal.size, t[1]-t[0])
     if(flag=="1"):
         graphics(FFT, freqs, signal, t,fs_rate)
@@ -56,3 +58,38 @@ def getFrequencyTimePlot(samplingFrequency,signalData):
     plt.xlabel('Time')
     plt.ylabel('Frequency')
     plt.show()
+
+
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+def filter(data,cutoff,fs,order):
+    # Demonstrate the use of the filter.
+    # First make some data to be filtered.
+    T = 5.0         # seconds
+    n = int(T * fs) # total number of samples
+    t = np.linspace(0, T, n, endpoint=False)
+    # "Noisy" data.  We want to recover the 1.2 Hz signal from this.
+    data = np.sin(1.2*2*np.pi*t) + 1.5*np.cos(9*2*np.pi*t) + 0.5*np.sin(12.0*2*np.pi*t)
+
+    # Filter the data, and plot both the original and filtered signals.
+    y = butter_lowpass_filter(data, cutoff, fs, order)
+
+    plt.subplot(2, 1, 2)
+    plt.plot(t, data, 'b-', label='data')
+    plt.plot(t, y, 'g-', linewidth=2, label='filtered data')
+    plt.xlabel('Time [sec]')
+    plt.grid()
+    plt.legend()
+
+    plt.subplots_adjust(hspace=0.35)
+    plt.show()
+
