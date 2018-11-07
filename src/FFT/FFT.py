b9adc80de772200c75cc12562bc07dc3cc571e6c
Nicole Reyes <31137695+nicoleStefanie@users.noreply.github.com>
2018-11-06 23:49:12 -0300

    Cambio en transformada, organizacion de graficos

--- src/FFT/FFT.py
+++ src/FFT/FFT.py
@@ -2,48 +2,128 @@
 from __future__ import print_function
 import scipy.io.wavfile as wavfile
 import scipy
-from scipy.signal import butter, lfilter, freqz
+from scipy.signal import butter, lfilter, freqz, firwin
 import scipy.fftpack
 import numpy as np
 from matplotlib import pyplot as plt
 
 
 
-def calculateFFT(fs_rate, signal, flag):
-    
-    print(flag)
-    l_audio = len(signal.shape)
-    print ("Channels", l_audio)
-    if l_audio == 2:
-        signal = signal.sum(axis=1) / 2
-    N = signal.shape[0]
-    print ("Complete Samplings N", N)
-    secs = N / float(fs_rate)
-    print ("secs", secs)
-    Ts = 1.0/fs_rate # sampling interval in time
-    print ("Timestep between samples Ts", Ts)
-    t = scipy.arange(0, secs, Ts) # time vector as scipy arange field / numpy.ndarray
-    FFT = abs(scipy.fft(signal))
-    filter(signal,3.667,30.0,7)
-    freqs = scipy.fftpack.fftfreq(signal.size, t[1]-t[0])
-    if(flag=="1"):
-        graphics(FFT, freqs, signal, t,fs_rate)
-    return 
+#################################
+# Calculo y gráfica de la transformada de fourier
+################################
 
-def graphics(FFT, freqs, signal, t,fs_rate):
-    plt.subplot(311)
-    p1 = plt.plot(t, signal, "g") # plotting the signal
-    plt.xlabel('Time')
-    plt.ylabel('Amplitude')
-    plt.subplot(312)
-    print(FFT)
-    p2 = plt.plot(freqs, FFT, "r") # plotting the complete fft spectrum
-    plt.xlabel('Frequency (Hz)')
-    plt.ylabel('Count dbl-sided')
-    plt.subplot(313)
+def calcFFT(fs_rate,signal):
+    fft = scipy.fft(signal)
+    fftNormalized = fft/len(signal)    
+    xfft = np.fft.fftfreq(len(fftNormalized),1/fs_rate)
+    return xfft, fftNormalized
+
+def plotTransform(xfft,fft):
+    plt.title("Transformada ")
+    plt.xlabel("Frecuency [Hz]")
+    plt.ylabel("Amplitude [dB]")
+    plt.plot(xfft,abs(fft))
+    plt.show()
+
+###################################
+# Calculo de paso bajo
+###################################
+
+def lowFilter(fs,cutoff,signal):
+    nyquist = fs/2
+    lowcut = cutoff
+    lowcut2 = lowcut/nyquist
+    numtaps = cutoff + 1
+    filter = firwin(numtaps,cutoff = lowcut2, window = 'hamming' )
+    filtered = lfilter(filter,1.0,signal)
+    len_signal = len(signal)#obtengo el largo de la señal
+    len_signal = float(len_signal)
+    time = float(len_signal)/float(fs)#genero el tiempo total del audio
+    x = np.arange(0,time,1.0/float(fs))
+    return x,filtered
+
+##################################
+# Grafica de señal vs tiempo
+##################################
+
+def plotSignalTime(signal,t):
+    # if(cplot%3 == 0):
+    #     cplot = 101 +cplot
+    #     plt.subplot(cplot)
+    #     plt.figure(cplot)
+    # else:
+    #     cplot = 101 +cplot
+    #     plt.subplot(cplot)
+
+    plt.plot(t,signal)
+    plt.title("Amplitud vs tiempo")
+    plt.xlabel("Tiempo [s]")
+    plt.ylabel("Amplitud [dB]")
+
+def plotTransform(xft,ft):
+    plt.title("Transformada ")
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel("Amplitud [dB]")
+    plt.plot(xft,abs(ft))
+
+##################################
+# Espectrograma
+##################################
+def plotSpec(signal,fs_rate):
+    plt.title("Espectrograma ")
     plt.specgram(signal,Fs=fs_rate)
-    plt.xlabel('Time')
-    plt.ylabel('Frequency')
+
+####################################
+# Mostrar todas las gráficas 
+####################################
+def getSignalTime(fs_rate,signal):
+    
+    signal_len = float(len(signal))
+    tAux = float(signal_len)/float(fs_rate)
+    t = np.linspace(0, tAux, signal_len)
+    return t
+def graphics(fs_rate,signal):
+    #Se obtiene la grafica del audio vs tiempo
+    plt.figure(1)
+    plotSignalTime(signal,getSignalTime(fs_rate,signal))
+    #Se obtiene la señal con el filtro paso bajo
+    plt.figure(2)
+    xFiltered,filteredSignal = lowFilter(fs_rate,1200,signal)
+    plotSignalTime(filteredSignal,xFiltered)
+    #Se obtiene la transformada de fourier
+    xfft,fft = calcFFT(fs_rate,signal)
+    #Se grafica la transformada del audio original
+    plt.figure(3)
+    plotTransform(xfft,fft)
+    #Se grafica la transformada del audio con filtro paso bajo
+    xlow,fftLow = calcFFT(fs_rate,filteredSignal)
+    plt.figure(4)
+    plotTransform(xlow,fftLow)
+
+    #Se grafica el espectrograma del audio original
+    plt.figure(5)
+    plotSpec(signal,fs_rate)
+    #Se grafica el espectrograma del audio con filtro paso bajo
+    plt.figure(6)
+    plotSpec(filteredSignal,fs_rate)
+    plt.show()
+    # plt.figure(1)
+    # plt.subplot(311)
+    # p1 = plt.plot(t, signal, "g") # plotting the signal
+    # plt.xlabel('Time')
+    # plt.ylabel('Amplitude')
+    # plt.subplot(312)
+    # p2 = plt.plot(freqs, FFT, "r") # plotting the complete fft spectrum
+    # # plt.subplot(313)
+    # # p3 = plt.specgram(signal,Fs=fs_rate)
+    # # plt.xlabel('Time')
+    # # plt.ylabel('Frequency')
+    # # plt.figure(2)
+    # plt.subplot(313)
+    # p4 = plt.plot(xFilter,filteredSignal,"g")
+    # plt.xlabel('dbs')
+    # plt.ylabel('Frequency')
     plt.show()
 
 def getFrequencyTimePlot(samplingFrequency,signalData):
@@ -66,30 +146,38 @@ def butter_lowpass(cutoff, fs, order=5):
     b, a = butter(order, normal_cutoff, btype='low', analog=False)
     return b, a
 
+
 def butter_lowpass_filter(data, cutoff, fs, order=5):
     b, a = butter_lowpass(cutoff, fs, order=order)
     y = lfilter(b, a, data)
     return y
 
-def filter(data,cutoff,fs,order):
-    # Demonstrate the use of the filter.
-    # First make some data to be filtered.
-    T = 5.0         # seconds
-    n = int(T * fs) # total number of samples
-    t = np.linspace(0, T, n, endpoint=False)
-    # "Noisy" data.  We want to recover the 1.2 Hz signal from this.
-    data = np.sin(1.2*2*np.pi*t) + 1.5*np.cos(9*2*np.pi*t) + 0.5*np.sin(12.0*2*np.pi*t)
-
-    # Filter the data, and plot both the original and filtered signals.
-    y = butter_lowpass_filter(data, cutoff, fs, order)
-
-    plt.subplot(2, 1, 2)
-    plt.plot(t, data, 'b-', label='data')
-    plt.plot(t, y, 'g-', linewidth=2, label='filtered data')
-    plt.xlabel('Time [sec]')
-    plt.grid()
-    plt.legend()
-
-    plt.subplots_adjust(hspace=0.35)
-    plt.show()
-
+"""
+def calculateFFT(fs_rate, signal, flag):
+    
+    print(flag)
+    l_audio = len(signal.shape)
+    print ("Channels", l_audio)
+    
+    if l_audio == 2:
+        signal = signal.sum(axis=1) / 2
+    
+    N = signal.shape[0]
+    print ("Complete Samplings N", N)
+    secs = N / float(fs_rate)
+    print ("secs", secs)
+    Ts = 1.0/fs_rate # sampling interval in time
+    print ("Timestep between samples Ts", Ts)
+    t = scipy.arange(0, secs, Ts) # time vector as scipy arange field / numpy.ndarra
+    #Filtro de la señal
+    xFilter,filteredSignal = lowFilter(fs_rate,1200,signal)
+    xfft, FFT = calcFFT(fs_rate,signal)
+    xFilter, FFT_filtered = calcFFT(xFilter,filteredSignal)
+    freqs = scipy.fftpack.fftfreq(signal.size, t[1]-t[0])
+    print("Filtered => \n")
+    print(filteredSignal)
+    #exit()
+    if(flag=="1"):
+        graphics(FFT, xfft, signal, t,fs_rate,FFT_filtered,xFilter)
+    return
+"""
